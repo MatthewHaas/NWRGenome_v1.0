17ce25554ac23d900dabaf928ce84f5b03d821b6
Matthew Haas <matthew.haas1@gmail.com>
2020-05-06 19:05:29 -0500

    Solved run error
    Gene IDs were not matching, so I fixed that. The next error that popped up was a dependency on LaTeX. So, I downloaded and installed that. Now, there are no errors! I still need to complete the code in order to produce the final figure.

--- run_jcvi.sh
+++ run_jcvi.sh
@@ -9,17 +9,18 @@
 cd /home/jkimball/haasx092/synteny_figure
 
 export PATH=$PATH:/home/jkimball/haasx092/synteny_figure/last-1060/src
+export PATH=$PATH:/home/jkimball/haasx092/synteny_figure/install-tl-20200505/1/bin/x86_64-linux
 
 module load python
 
 python -m jcvi.formats.gff bed rice.gene_structures_post_PASA_updates.21917.gff3 -o wild_rice.bed
 python -m jcvi.formats.gff bed Osativa_323_v7.0.gene.gff3.gz -o oryza.bed
 
-python -m jcvi.formats.fasta format zizania_palustris_13Nov2018_okGsv.fasta wild_rice.cds
+python -m jcvi.formats.fasta format rice.gene_structures_post_PASA_updates.nucleotides.fsa wild_rice.cds
 python -m jcvi.formats.fasta format Osativa_323_v7.0.cds.fa.gz oryza.cds
 
-sed -i  's/\./_/g' oryza.bed
-sed -i  's/\./_/g' oryza.cds
-sed -i 's/;.*$//g' wild_rice.cds
+sed -i  's/\.MSUv7.0//g' oryza.bed
+sed -i  's/\..*$//g' oryza.cds
+sed -i 's/\-.*$//g' wild_rice.cds
 
 python -m jcvi.compara.catalog ortholog wild_rice oryza --no_strip_names
