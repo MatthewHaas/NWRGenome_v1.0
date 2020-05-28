ab053efda6082ccf8f65a703cd2915e368089886
Matthew Haas <matthew.haas1@gmail.com>
2020-05-27 20:26:15 -0500

    Updated figure
    Removed cutgrass, added _Brachypodium_ and _Sorghum_. Put species names in italics.

--- venn_diagram_orthogroups.R
+++ venn_diagram_orthogroups.R
@@ -14,25 +14,30 @@ O_sativa <- x[Oryza_sativa > 0]
 Z_palustris <- x[Zizania_palustris > 0]
 Z_mays <- x[Zea_mays > 0]
 L_perrieri <- x[Leersia_perrieri > 0]
+B_distachyon <- x[Brachypodium_distachyon > 0]
+S_bicolor <- x[Sorghum_bicolor > 0]
 
 # Get the orthogroup IDS for each species.
 Osativa_orthogroups <- O_sativa$Orthogroup
 Zpalustris_orthogroups <- Z_palustris$Orthogroup
 Zmays_orthogroups <- Z_mays$Orthogroup
 Lperrieri_orthogroups <- L_perrieri$Orthogroup
+Bdistachyon_orthogroups <- B_distachyon$Orthogroup
+Sbicolor_orthogroups <- S_bicolor$Orthogroup
 
 # Save data
-save(Osativa_orthogroups, Zpalustris_orthogroups, Lperrieri_orthogroups, Zmays_orthogroups, file="orthogroup_names_for_venn_diagram.Rdata")
+save(Osativa_orthogroups, Zpalustris_orthogroups, Lperrieri_orthogroups, Zmays_orthogroups, Bdistachyon_orthogroups, Sbicolor_orthogroups, file="orthogroup_names_for_venn_diagram.Rdata")
 
 # In RStudio now... I could not get the venn.diagram function to work. Could not write to PNG for some reason.
 setwd("~/Documents/wild_rice")
 load("orthogroup_names_for_venn_diagram.Rdata") # File contains sets of Orthogroups to feed into the venn.diagram function.
 
-colors= c("#6b7fff", "#c3db0f", "#ff4059", "#2cff21")
+colors= c("#6b7fff", "#c3db0f", "#ff4059", "#2cff21", "#de4dff")
 
 # Make the Venn Diagram
-venn.diagram(x=list(Osativa_orthogroups, Zpalustris_orthogroups, Zmays_orthogroups, Lperrieri_orthogroups),
-			 category.names = c("O. sativa", "Z. palustris", "Z. mays", "L. perrieri"),
+venn.diagram(x=list(Osativa_orthogroups, Zpalustris_orthogroups, Zmays_orthogroups, Bdistachyon_orthogroups, Sbicolor_orthogroups),
+			 category.names = c(expression(italic("O. sativa")), expression(italic("Z. palustris")), expression(italic("Z. mays")), expression(italic("B. distachyon")),
+			 expression(italic("S. bicolor"))),
 			 filename = "test_venn_diagram.png",
 			 output=TRUE,
 			 imagetype="png",
@@ -41,4 +46,5 @@ venn.diagram(x=list(Osativa_orthogroups, Zpalustris_orthogroups, Zmays_orthogrou
 			 fill = colors,
 			 cat.col = colors,
 			 cat.cex = 1,
+			 cat.dist = 0.26,
 			 margin = 0.15)
